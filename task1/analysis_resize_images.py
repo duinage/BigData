ee96cac51c13d34a7885e6d4763a0fce9c32093c
Vadym <tunik.vadym@gmail.com>
2025-03-31 15:34:59 +0300

    task1 done

---
+++ task1/analysis_resize_images.py
@@ -0,0 +1,27 @@
+import subprocess
+import time
+import matplotlib.pyplot as plt
+
+execution_times = []
+process_counts = range(1, 11)
+
+for n in process_counts:
+    command = f"mpiexec -n {n} python resize_images.py"
+    print(f"Running: {command}")
+    
+    start_time = time.perf_counter()
+    result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+    total_time = time.perf_counter() - start_time
+
+    execution_times.append(total_time)
+    print(f"time for {n} processes: {total_time:.2f} s.")
+
+
+plt.figure()
+plt.plot(process_counts, execution_times, marker='o', linestyle='-', color='b')
+plt.title("Execution Time vs. Number of Processes")
+plt.xlabel("Number of Processes")
+plt.ylabel("Time (seconds)")
+plt.grid(True)
+plt.savefig("time_vs_processes.png")
+plt.show()
